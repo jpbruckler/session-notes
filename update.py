d75f38325bf8cd5eabe5a8b6d342e8c7f6224723
John Bruckler <jpbruckler@gmail.com>
2022-09-25 23:15:30 -0400

    updating to add table support

--- update.py
+++ update.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python3
+
 from pathlib import Path
 import shutil
 import yaml
 import re
+import colorama
+
+def progressBar(value, endvalue, bar_length=20):
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+
+    print("Progress: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))), end='\r')
+
 
 def replaceAdmonition(filepath, flags=0):
     adOpen = re.compile(r'\s{0,1}>\s\[!(\w+)\]([-\+\s]*)(.*)', flags)
